0f98d0bb2d574381432627d7484ab2317a4f273e
official-anato <139602951+official-anato@users.noreply.github.com>
2026-04-19 15:20:27 +0800

    Updated assembler.
    The assembler is mostly close to it's core completion, but the way it generates MD bytes are wrong, and the parameters are being written in reverse.

--- mainfile-assembler-v2.cpp
+++ mainfile-assembler-v2.cpp
@@ -75,19 +75,82 @@ int main(int argc, char* argv[]) {
       std::vector<std::string> parameters;
       std::string line_copy = inst;
       line_copy.erase(0, 4);
+
+      // Find all mentions of a comment, and get rid of them.
       size_t commentpos = line_copy.find('!');
       if (commentpos != std::string::npos) {
         line_copy.erase(commentpos, line_copy.size());
       }
 
-      size_t commapos = line_copy.find(',');
-      if (commapos != std::string::npos) {
-        parameters.push_back(line_copy.substr(0, commapos));
-        line_copy.erase(0, commapos + 1);
-        parameters.push_back(line_copy);
+      // Put each parameter into a vector.
+      while (!line_copy.empty()){
+        size_t spacepos = line_copy.find(" ");
+        if (spacepos != std::string::npos){
+          if (spacepos > 0) {  // Avoid empty parameters
+            parameters.push_back(line_copy.substr(0, spacepos));
+          }
+
+          line_copy.erase(0, spacepos + 1);  // Remove param and the space
+        }
+
+        else {
+          if (!line_copy.empty()) {
+            parameters.push_back(line_copy);  // Last parameter
+          }
+
+          break;
+        }
       }
 
-      for (std::string item : parameters){
+      // MD Byte Generation
+      // Create the dictionary for determining if a parameter is immediate, a register, or an address.
+      std::map<std::string, std::string> MD_DETERMINE = {
+        {"imm", "0b00"},
+        {"reg", "0b01"},
+        {"addr", "0b10"}
+      };
+      // Create the final output for the MD Byte
+      std::vector<std::string> MD_final = {};
+
+      // Iterate through each item in vector,
+      // Check the prefix for 'R', '@', or none
+      // (Refer to the MD_DETERMINE dict),
+      // Append the determined prefix to MD_final
+      
+      std::vector<std::string> REV_parameters(parameters.rbegin(), parameters.rend());
+      std::vector<std::string> binary_parameters;
+      for (const std::string item : REV_parameters){
+        std::string binaryitem;
+        if (item.substr(0,1) == "R"){
+          MD_final.push_back(MD_DETERMINE.at("reg"));
+          binaryitem = std::bitset<8>(std::stoi(item.substr(1))).to_string();
+        }
+
+        else if (item.substr(0,1) == "@"){
+          MD_final.push_back(MD_DETERMINE.at("addr"));
+          binaryitem = std::bitset<8>(std::stoi(item.substr(1))).to_string();
+        }
+
+        else{
+          MD_final.push_back(MD_DETERMINE.at("imm"));
+          binaryitem = std::bitset<8>(std::stoi(item)).to_string(); // Why does it make stoi not work??
+        }
+        
+        binary_parameters.push_back(binaryitem);
+      }
+
+      // Join MD_final into one string
+      std::string MD_output = "";
+      for (const std::string& item : MD_final){
+        MD_output += item;
+      }
+
+      std::vector<std::string> parameters_final = {};
+      parameters_final.push_back("0b" + MD_output);
+      for (const std::string& item : binary_parameters){
+        parameters_final.push_back(item);
+      }
+      for (std::string item : parameters_final){
         output.push_back(item);
       }
       
