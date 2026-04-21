34e3a94df5d8e8a840abb010a4d98e70a9866dfb
official-anato <139602951+official-anato@users.noreply.github.com>
2026-04-21 10:50:50 +0800

    Added previous existing functions into CPU32 according to documentation.

--- CPU32.cpp
+++ CPU32.cpp
@@ -14,48 +14,75 @@
 #include <functional>
 
 class memory{
-  std::vector<uint8_t> RAM = std::vector<uint8_t>(65535, 0);
-  // These two functions will be written later.
-  // For now they will be boilerplate.
-  // A blueprint if you will.
-  void write(const uint8_t& start_addr, const std::vector<uint8_t>& data){
-  }
+  public:
+    std::vector<uint8_t> RAM = std::vector<uint8_t>(65535, 0);
+    // These two functions will be written later.
+    // For now they will be boilerplate.
+    // A blueprint if you will.
+    void write(const uint8_t& start_addr, const std::vector<uint8_t>& data){
+      int stop_byte = data.size();
+      int current_byte = 0;
+      while (current_byte < stop_byte){
+        RAM.insert(RAM.begin() + start_addr + current_byte, data.begin(), data.end());
+        current_byte++;
+      }
+    }
+    
+    std::vector<uint8_t> read(const uint8_t& start_addr, const uint8_t& stop_addr){
 
-  std::vector<uint8_t> read(const uint8_t& start_addr, const uint8_t& stop_addr){
-    std::vector<uint8_t> data;
-    return data;
-  }
+      std::vector<uint8_t> data;
+      return data;
+    }
 };
 
 class reg{
-  std::vector<uint8_t> registers;
-  void write(const uint8_t* data){
-  }
+  public:
+    std::vector<uint8_t> registers;
+    void write(const uint8_t* data){
+    }
 
-  std::vector<uint8_t> read(const uint8_t& start_addr, const uint8_t& stop_addr){
-    std::vector<uint8_t> data;
-    return data;
-  }
+    std::vector<uint8_t> read(const uint8_t& start_addr, const uint8_t& stop_addr){
+      std::vector<uint8_t> data;
+      return data;
+    }
 };
 
-class kernel{
-  void read(const uint8_t& params){} // addr, data, byte count
-  void user_input(const uint8_t& params){} // input src, writing address
-  void filewrite(const uint8_t& params){} // filename, data
-  void fileread(const uint8_t& params){} // filename, byte count
-  std::map<std::string, std::function<void(uint8_t params)>> a;
-};
-
-class SDL_GRAPHICS{};
+class SDL_GRAPHICS{}; // Currently empty class to be filled in later.
 
 class CPU{
   private:
+    void kernel_print(){} // addr, data, byte count
+    void kerne_userinput(){} // input src, writing address
+    void kerne_filewrite(){} // filename, data
+    void kerne_fileread(){} // filename, byte count
+
     memory RAM;
     reg Register;
-    kernel Kernel;
+    bool Zero = false;
+    bool Carry = false;
+    bool Sign = false;
+    int PC = 0;
+
   public:
-    void run(const bool& logging, const std::vector<uint8_t> prg){
-      // Code
+    void add(){}
+    void sub(){}
+    void mul(){}
+    void div(){}
+    void mod(){}
+    void jmp(){}
+    void jeq(){}
+    void jlt(){}
+    void jgt(){}
+    void cmp(){}
+    void interrupt(){}
+    void halt(){}
+    void sdl_graphics(){}
+    void ens(){}
+    void mov(){}
+    void run(const bool& logging, const std::vector<uint8_t>& PRG){
+      bool running = true;
+      while (running && PC < (int)PRG.size()){
+      }
     }
 };
 
